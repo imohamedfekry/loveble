8ab8041025f1fe998f51b3593590cbbb396ef1cd
Dev.Muhammed3 <muhdid82@gmail.com>
2026-09-13 10:06:08 +0300

    fix(bootstrap): rename app.bootstrap.TS to lowercase and clean drizzle config

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
+export default {
   schema: './src/common/database/schema/**/*.ts',
   out: './src/common/database/migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+};
